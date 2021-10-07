06dc28c2b34184972db64f9a2b2cc024edfa24eb
savka-olia <tokarchykolia@gmail.com>
2021-10-07 12:59:30 +0300

    gh-pages

---
+++ build/precache-manifest.9e3c9827a660a81580dc1c571c2959be.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cf228830005e5e54c0c370ac13796d",
+    "url": "/chat_app/index.html"
+  },
+  {
+    "revision": "c99e942ef451166084ab",
+    "url": "/chat_app/static/css/main.0968772f.chunk.css"
+  },
+  {
+    "revision": "aef228f76889f35b53cc",
+    "url": "/chat_app/static/js/2.b22b7c30.chunk.js"
+  },
+  {
+    "revision": "c99e942ef451166084ab",
+    "url": "/chat_app/static/js/main.94627b77.chunk.js"
+  },
+  {
+    "revision": "562f21b1f7facb51afa9",
+    "url": "/chat_app/static/js/runtime-main.15104ce5.js"
+  },
+  {
+    "revision": "795342bb7456ea6bc13d0cda16d6a83f",
+    "url": "/chat_app/static/media/chika.795342bb.png"
+  },
+  {
+    "revision": "dec22100b719508910cfcd18a36a909c",
+    "url": "/chat_app/static/media/dio.dec22100.jpg"
+  },
+  {
+    "revision": "d821006e740d5e5ed58ff8afb74eaa5b",
+    "url": "/chat_app/static/media/jotaro.d821006e.jpg"
+  },
+  {
+    "revision": "44d3b27b4f4ad78d3b6eeccae8c01ef4",
+    "url": "/chat_app/static/media/search.44d3b27b.svg"
+  },
+  {
+    "revision": "f6a99aefe2d07d99cd90a9f6359c30f1",
+    "url": "/chat_app/static/media/umaru_chan.f6a99aef.jpg"
+  }
+]);
